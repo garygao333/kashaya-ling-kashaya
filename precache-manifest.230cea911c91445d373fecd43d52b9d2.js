8f32c71ad34ac2329da7fc1bc5a8ad3242ceb223
Gary Gao <>
2024-12-30 14:30:24 -0500

    Updates

---
+++ precache-manifest.230cea911c91445d373fecd43d52b9d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1010c9b36b948e5e4c3b0001021a1c48",
+    "url": "/kashaya-ling-kashaya/index.html"
+  },
+  {
+    "revision": "7c600d036f7d69f72ba2",
+    "url": "/kashaya-ling-kashaya/static/css/main.96e59dc3.chunk.css"
+  },
+  {
+    "revision": "dd2d1f0870069d2fd2b0",
+    "url": "/kashaya-ling-kashaya/static/js/2.211b624b.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/kashaya-ling-kashaya/static/js/2.211b624b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c600d036f7d69f72ba2",
+    "url": "/kashaya-ling-kashaya/static/js/main.ad87d519.chunk.js"
+  },
+  {
+    "revision": "13e8e4599207d3b24753",
+    "url": "/kashaya-ling-kashaya/static/js/runtime-main.18629a3e.js"
+  },
+  {
+    "revision": "1619f5f30e024f7870054ed4259d382b",
+    "url": "/kashaya-ling-kashaya/static/media/background.1619f5f3.jpg"
+  }
+]);
